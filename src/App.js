56c263f702300cac0e7b2f328a93c7444e08fcfc
rizaievdima <rizaievdima@gmail.com>
2025-05-08 10:16:37 +0300

    first component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import FirstComponent from "./components/FirstComponent";
+
 function App() {
     return (
         <div className="App">
-            <h1>App</h1>
+            <FirstComponent />
         </div>
     );
 }
